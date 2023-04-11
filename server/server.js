3d7f4eecc9d4b0882116b58ff24c47ff73b3f82b
anderson-matheus <matheus5347@gmail.com>
2023-04-10 23:57:10 -0300

    add controler for api football

--- server/server.js
+++ server/server.js
@@ -1,25 +1,11 @@
 const express = require('express')
 const app = express()
-const request = require('request')
+
 const dotenv = require('dotenv')
 dotenv.config()
-const { chunk } = require('./utils')
 
-app.get("/api/matches", (req, res) => {
-    const options = {
-        'method': 'GET',
-        'url': `${process.env.API_URL}/competitions/2013/matches`,
-        'headers': {
-            'X-Auth-Token': process.env.TOKEN
-        }
-    }
-    request(options, function (error, response) {
-        if (error) throw new Error(error)
-        const body = JSON.parse(response.body)
-        const matches  = body.matches
-        const matchdays = chunk(matches, 10)
-        res.json(matchdays[4])
-    })
-})
+apiFootballController = require('./controllers/apiFootballController')
+
+app.get("/api/matches", apiFootballController.getMatches)
 
 app.listen(5000, () => { console.log("server started on port 5000") })
